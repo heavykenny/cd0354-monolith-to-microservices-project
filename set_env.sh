15ef3bec31340df4538529741992052d99ab2869
Kehinde Olawuwo <olawuwokenny@gmail.com>
2022-09-16 20:07:14 +0100

    Adding Circle CI

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=BWYt8bULhDAgGnj
+export POSTGRES_HOST=postgres.c0diryduuk5u.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udacity-k8s-s3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=udacity
+export JWT_SECRET=testing
+export URL=http://localhost:8100
